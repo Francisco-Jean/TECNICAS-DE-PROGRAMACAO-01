00db7c170ea91efdb45517cf8a18869e5b56219c
Francisco Jean <71938841+Francisco-Jean@users.noreply.github.com>
2023-04-11 09:05:27 -0300

    INCREMENTO DOS ARQUIVOS UTILIZADOS NA AULA DO DIA 11/04

---
+++ ARQUIVOS-LABORATORIO/Pessoa.java
@@ -0,0 +1,24 @@
+abstract class Pessoa {
+    private String Nome;
+    private int idade;
+    private char sexo;
+
+    Pessoa (){
+        this("Sem nome", 0, ' ');
+    }
+
+    Pessoa (String nome){
+        this(nome, 0, ' ');
+    }
+
+    Pessoa (String nome, int idade){
+        this(nome, idade, ' ');
+    }
+
+    Pessoa (String nome, int idade, char sexo){
+        this(nome, idade, sexo);
+    }
+
+    
+
+}
